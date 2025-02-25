cd85d914c037b5edb25666e17c5e35c923be7841
Pallabis52 <pallabisahu58@gmail.com>
2025-02-25 18:52:41 +0530

    add template on my homepage

--- EXPENSE_FRONTENT/src/components/Home/Home.jsx
+++ EXPENSE_FRONTENT/src/components/Home/Home.jsx
@@ -1,18 +1,39 @@
 
 "use client";
 
-import { Button, Card, Footer } from "flowbite-react";
+// import { Button, Card, Footer } from "flowbite-react";
 import Header from "../Header/Header";
 import Dashboard from "../Admin/Dashboard/Dashboard";
 import FooterComponent from "../Footer/Footer";
-
-
+import { Button } from "flowbite-react";
 
 export default function Home() {
   return (
    <>
      <Header />
-        <Dashboard/>
+     <div className="flex h-screen">
+      <div className="w-1/2 flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center h-screen gap-4">
+          <div className="p-5">
+            <p className="text-lg font-bold">
+              An Expense Management System helps individuals or businesses
+              track,<br></br> manage, and analyze their expenses efficiently. It
+              automates expense reporting.
+            </p>
+          </div>
+          <div className="p-5">
+            <Button>Get Started</Button>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 flex items-center justify-center">
+        <img
+          src="https://cdni.iconscout.com/illustration/premium/thumb/budget-management-illustration-download-in-svg-png-gif-file-formats--expense-managing-accounting-finance-statement-planning-isometric-pack-professionals-illustrations-4040951.png"
+          alt="Example"
+        />
+      </div>
+    </div>
+
         <FooterComponent />
    </>
   );
